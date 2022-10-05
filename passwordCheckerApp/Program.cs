33ae1a5f989d40768c907a7100854b936a481230
[traciaw] <traciawee@gmail.com>
2022-10-06 01:20:20 +0800

    improved loop for retry

--- passwordCheckerApp/Program.cs
+++ passwordCheckerApp/Program.cs
@@ -1,87 +1,95 @@
 ﻿using System;
 
 class MainClass
-{
-    public static void Main(string[] args)
+{    public static void Main(string[] args)
     {
-        //password requirements
-        int minLength = 9;
-        string uppercase = "ABCDEFGHIKLMNOPQRSTVXYZ";
-        string lowercase = "abcdefghiklmnopqrstvxyz";
-        string digits = "0123456789";
-        string specialChars = "!@\"'()*=+,-./";
+        bool repeat;
+        char newinput;
 
-        //user input
-        Console.Write("Enter a password: ");
-        string userInput = Console.ReadLine();
+        do
+        {
+            //password requirements
+            int minLength = 9;
+            string uppercase = "ABCDEFGHIKLMNOPQRSTVXYZ";
+            string lowercase = "abcdefghiklmnopqrstvxyz";
+            string digits = "0123456789";
+            string specialChars = "!@\"'()*=+,-./";
 
-        //refactor input length
-        int passwordLength = userInput.Length;
-
-        //password strength score
-        int score = 0;
-        if (passwordLength >= minLength)
-        {
-            score = score + 1;
-            //Console.WriteLine($"Score: {score}");
-        }
-        if (Contains(userInput, uppercase))
-        {
-            score = score + 1;
-            //Console.WriteLine($"Score: {score}");
-        }
-        if (Contains(userInput, digits))
-        {
-            score = score + 1;
-            //Console.WriteLine($"Score: {score}");
-        }
-        if (Contains(userInput, specialChars))
-        {
-            score = score + 1;
-            //Console.WriteLine($"Score: {score}");
-        }
-        if (userInput == "password")
-        {
-            score = 0;
-            Console.WriteLine("This password is too easy to crack!");
-            //Console.WriteLine($"Score: {score}");
-        }
-        if (userInput == "1234")
-        {
-            score = 0;
-            Console.WriteLine("This password is too easy to crack!");
-            //Console.WriteLine($"Score: {score}");
-        }
-        Console.WriteLine($"Score: {score}");
-
-        //CHANGE TO ACCEPT >4
-        //REQUIRE TO REINPUT IF REQUIREMENTS NOT MET
-        switch (score)
-        {
-            case 4:
-                Console.WriteLine("This is an extremely strong password");
-                break;
-            case 3:
-                Console.WriteLine("This is a strong password");
-                break;
-            case 2:
-                Console.WriteLine("This is an average password");
-                break;
-            case 1:
-                Console.WriteLine("This is a weak password");
-                break;
-            default:
-                Console.WriteLine("This password doesn't meet the standards");
-                break;
-        }
-        Console.ReadLine();
-
-        if (score < 3)
-        {
-            Console.WriteLine("Try Again");
+            //user input
             Console.Write("Enter a password: ");
+            string userInput = Console.ReadLine();
+
+            //refactor input length
+            int passwordLength = userInput.Length;
+
+            //password strength score
+            int score = 0;
+            {
+                if (passwordLength >= minLength)
+                {
+                    score = score + 1;
+                    //Console.WriteLine($"Score: {score}");
+                }
+                if (Contains(userInput, uppercase))
+                {
+                    score = score + 1;
+                    //Console.WriteLine($"Score: {score}");
+                }
+                if (Contains(userInput, digits))
+                {
+                    score = score + 1;
+                    //Console.WriteLine($"Score: {score}");
+                }
+                if (Contains(userInput, specialChars))
+                {
+                    score = score + 1;
+                    //Console.WriteLine($"Score: {score}");
+                }
+                if (userInput == "password")
+                {
+                    score = 0;
+                    Console.WriteLine("This password is too easy to crack!");
+                    //Console.WriteLine($"Score: {score}");
+                }
+                if (userInput == "1234")
+                {
+                    score = 0;
+                    Console.WriteLine("This password is too easy to crack!");
+                    //Console.WriteLine($"Score: {score}");
+                }
+                Console.WriteLine($"Score: {score}");
+
+                //CHANGE TO ACCEPT >4
+                //REQUIRE TO REINPUT IF REQUIREMENTS NOT MET
+                switch (score)
+                {
+                    case 4:
+                        Console.WriteLine("This is an extremely strong password");
+                        break;
+                    case 3:
+                        Console.WriteLine("This is a strong password");
+                        break;
+                    case 2:
+                        Console.WriteLine("This is an average password");
+                        break;
+                    case 1:
+                        Console.WriteLine("This is a weak password");
+                        break;
+                    default:
+                        Console.WriteLine("This password doesn't meet the standards");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Would you like to retry? Input: Y + ENTER key ");
+            newinput = Convert.ToChar(Console.ReadLine());
 
         }
+        while (newinput == 'y');
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+        
     }
     
 
